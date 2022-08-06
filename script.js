9b2aa78def8e233069ec3f23171e5422a780e9bc
Szymon Gryz <szymongryz@interia.pl>
2022-08-06 22:15:06 +0200

    function for appearing window with report script.js

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ document.querySelector('#close-sign').addEventListener('click',
 function() {
     document.querySelector('#post-options').style.display = 'none'
 })
+
+document.querySelector('#report').addEventListener('click',
+function() {
+    document.querySelector('#report-background').style.display = 'flex'
+}) 
+
+document.querySelector('#report-background').addEventListener('click',
+function() {
+    document.querySelector('#report-background').style.display = 'none'
+})
