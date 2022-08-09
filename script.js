baee1042b83bbfd38a6f4eb30dcb5831dd1a5376
Szymon Gryz <szymongryz@interia.pl>
2022-08-09 15:53:14 +0200

    like animation script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// report window posts-container
+
 document.querySelector('#button').addEventListener('click',
 function() {
     document.querySelector('#post-options').style.display = 'block'
@@ -18,6 +20,10 @@ function() {
     document.querySelector('#report-background').style.display = 'none'
 })
 
+/**/
+
+/* messages window at index.html */ 
+
 document.getElementById('message').addEventListener('click',
 function() {
     document.querySelector('#notification').style.display = 'none'
@@ -28,3 +34,16 @@ document.querySelector('#close-messages').addEventListener('click',
 function() {
     document.querySelector('#messages').style.display = 'none'
 })
+
+document.querySelector('#message').addEventListener('click',
+function() {
+    document.querySelector('#message-content').style.fontWeight = 400
+})
+
+/* like animation */
+
+const likesList = document.getElementsByClassName('fa-thumbs-up')
+
+for (let i = 0; i < likesList.length; i++) {
+    likesList[i].addEventListener('click', () => likesList[i].classList.toggle('animation-like'))
+}
