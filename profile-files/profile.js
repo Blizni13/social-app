03b27879e73a5d65d484da8bdf05f18a334def0d
Szymon Gryz <szymongryz@interia.pl>
2022-08-14 15:53:24 +0200

    posts-section from index.html page to profile.html with js and css settings

---
+++ profile-files/profile.js
@@ -0,0 +1,25 @@
+// REPORT WINDOW
+
+const reportButton = document.getElementsByClassName('fa-ellipsis')
+const postOptions = document.getElementsByClassName('post-options') 
+const closeSign = document.getElementsByClassName('close-sign')
+const reportText = document.getElementsByClassName('report')
+
+for (let z = 0; z < reportButton.length; z++) {
+    reportButton[z].addEventListener('click', () => postOptions[z].style.display ='block')
+    closeSign[z].addEventListener('click', () => postOptions[z].style.display = 'none')
+    reportText[z].addEventListener('click', () => document.querySelector('#report-background').style.display = 'flex')
+}
+
+document.querySelector('#report-background').addEventListener('click',
+function() {
+    document.querySelector('#report-background').style.display = 'none'
+})
+
+// LIKE ANIMATION
+
+const likesList = document.getElementsByClassName('fa-thumbs-up')
+
+for (let i = 0; i < likesList.length; i++) {
+    likesList[i].addEventListener('click', () => likesList[i].classList.toggle('animation-like'))
+}
