8d0773b8b9622d35187a409258edc0e6775aa2c9
Szymon Gryz <szymongryz@interia.pl>
2022-08-15 15:43:53 +0200

    script.js - section-click effects

--- profile-files/profile.js
+++ profile-files/profile.js
@@ -23,3 +23,13 @@ const likesList = document.getElementsByClassName('fa-thumbs-up')
 for (let i = 0; i < likesList.length; i++) {
     likesList[i].addEventListener('click', () => likesList[i].classList.toggle('animation-like'))
 }
+
+// SECTIONS SELECT ANIMATIONS
+
+let posts = document.getElementById("posts")
+let photos = document.getElementById("photos")
+let about = document.getElementById("about")
+
+posts.addEventListener('click', () => document.getElementById("posts-underline").style.display = 'block')
+photos.addEventListener('click', () => document.getElementById("photos-underline").style.display = 'block')
+about.addEventListener('click', () => document.getElementById('about-underline').style.display = 'block')
